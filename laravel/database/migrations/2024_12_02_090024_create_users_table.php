970bfd4360c80e2234cab5a7333bd3bdfd008486
fh95-tech <fakih.hussein995@gmail.com>
2024-12-02 11:05:56 +0200

    added columns to users table

--- laravel/database/migrations/2024_12_02_090024_create_users_table.php
+++ laravel/database/migrations/2024_12_02_090024_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('requests_num');
             $table->timestamps();
         });
     }
