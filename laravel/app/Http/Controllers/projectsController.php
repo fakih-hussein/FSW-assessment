9dc00a8efc180736d7bcb90a35b3baa55cb71c18
fh95-tech <fakih.hussein995@gmail.com>
2024-12-02 11:25:13 +0200

    modified the functions

--- laravel/app/Http/Controllers/projectsController.php
+++ laravel/app/Http/Controllers/projectsController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class projectsController extends Controller
 {
-    //
+     function getProjects(){
+        $projects=Project::All();
+
+        return response()->json([
+            "projects"=>$projects,
+        ]);
+    }
+
+     function getProject($id){
+        $project=Project::find($id);
+
+        return response()->json([
+            "project"=>$project,
+        ]);
+    }
+
+    
 }
