67a216bc3dfcc23e5f2bab5609475fdbf24efc26
fh95-tech <fakih.hussein995@gmail.com>
2024-12-02 11:08:12 +0200

    added foreign key columns to the pivot table

--- laravel/database/migrations/2024_12_02_090120_create_project_user_table.php
+++ laravel/database/migrations/2024_12_02_090120_create_project_user_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('project_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
